33fa415f486ff71a33a939c20a4745fa79cfe94a
Henry-3000 <chukwumaezehenry15@gmail.com>
2023-09-22 07:43:56 +0100

    fix

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import 'react-toastify/dist/ReactToastify.css'
 import ImageGal from "./Pages/ImageGal"
-import Login from "./Login"
+import Login from "./Pages/Login"
 import { ToastContainer } from 'react-toastify';
 
 
